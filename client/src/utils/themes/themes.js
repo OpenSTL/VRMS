c67c18b49445e84956e0d2a19b0a6c9f98f7b890
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-19 12:24:57 -0500

    add constants to manage brigade references used in app

---
+++ client/src/utils/themes/themes.js
@@ -0,0 +1,7 @@
+// Brigade Details
+export const BRIGADE_NAME = "Open STL";
+export const BRIGADE_EMAIL = "info@openstl.org";
+export const BRIGADE_SITE_URL = "https://openstl.org/";
+export const BRIGADE_SITE_NAME = "openstl.org";
+export const BRIGADE_VRMS_REPO = "https://github.com/OpenSTL/VRMS";
+export const BRIGADE_SLACK = "https://openstl.slack.com";
